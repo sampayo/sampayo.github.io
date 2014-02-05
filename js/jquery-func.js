a2768871a0a99b9dd5aaf8df2ed102d6c1561ec2
Ricardo Sampayo <RicardoSampayo@MacBook-Pro-de-Ricardo.local>
2014-02-04 23:29:13 -0430

    fix acordeon

--- js/jquery-func.js
+++ js/jquery-func.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 		$('.accordion-trigger:first').addClass('active').next().show();
 		$('.accordion-trigger').click(function(){
 			if( $(this).next().is(':hidden') ) { 
-				$('.accordion-trigger').removeClass('active').next().slideUp();
+				$(this).parent().find('.accordion-trigger').removeClass('active').next().slideUp();
 				$(this).toggleClass('active').next().slideDown();
 			}
 			return false;
